6b517e3fd169018310cfce0c1f69f96f9fa30dbc
Shawn C. Lilly <shawnlilly00@yahoo.com>
2020-09-24 10:17:32 -0600

    ta02

--- cse341-project-master/cse341-project-master/routes/ta02.js
+++ cse341-project-master/cse341-project-master/routes/ta02.js
@@ -1,15 +1,33 @@
 //TA02 PLACEHOLDER
-// Remember, you can make more of these placeholders yourself! 
+
 const express = require('express');
 const router = express.Router();
+
+const users = [];
+
+router.post('/addUser',(req, res, next) => {
+        const newUser = req.body.newUser;
+        users.push(newUser);
+        res.redirect('/ta02/');
+    });
+
+router.post('/removeUser',(req, res, next) => {
+    const rUser = req.body.rUser;
+    const index = users.indexOf(rUser);
+    if (index !== -1 ) {
+        users.splice(index, 1);
+    }
+
+    res.redirect('/ta02/');
+});
 
 router.get('/',(req, res, next) => {
     res.render('pages/ta02', { 
         title: 'Team Activity 02', 
+        users: users,
         path: '/ta02', // For pug, EJS 
         activeTA03: true, // For HBS
         contentCSS: true, // For HBS
     });
 });
-
 module.exports = router;
